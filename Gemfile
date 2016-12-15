916ea96b57f98d80af497cdb8004e473889e3ac7
Leone Parise <leone.parise@gmail.com>
2016-12-14 22:43:08 -0200

    New project structure

--- Gemfile
+++
@@ -1,6 +0,0 @@
-source 'https://rubygems.org'
-
-gem 'naturally'
-gem 'rake'
-gem 'xcpretty'
-gem 'cocoapods'
---
+++ ParseFacebookUtils/ParseFacebookUtils.h
@@ -0,0 +1,19 @@
+//
+//  ParseFacebookUtils.h
+//  ParseFacebookUtils
+//
+//  Created by Leone Parise Vieira da Silva on 14/12/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ParseFacebookUtils.
+FOUNDATION_EXPORT double ParseFacebookUtilsVersionNumber;
+
+//! Project version string for ParseFacebookUtils.
+FOUNDATION_EXPORT const unsigned char ParseFacebookUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ParseFacebookUtils/PublicHeader.h>
+
+#import <ParseFacebookUtils/PFFacebookUtils.h>
